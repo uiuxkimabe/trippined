f37bc5c15d10300af24a72ac8a307edbe827b6b2
uiuxkimabe <uiuxkimabe@gmail.com>
2023-11-08 07:17:35 +0700

    rooms and price display price

--- src/js/index.js
+++ src/js/index.js
@@ -4,20 +4,23 @@ const priceWe = document.querySelectorAll(".priceWe");
 const rmCard = document.querySelectorAll(".rm-card");
 
 // desctructuring price dan room card
-let [prWdA, prWdB, prWdC, prWdD] = priceWd;
-let [prWeA, prWeB, prWeC, prWeD] = priceWe;
-let [villaA, villaB, villaC, villaD] = rmCard;
+let [prWdA, prWdB, prWdC, prWdD] = priceWd; // Harga WeekDay
+let [prWeA, prWeB, prWeC, prWeD] = priceWe; // Harga Week End
+let [villaA, villaB, villaC, villaD] = rmCard; //Nama Villa
 
 // Display Harga di Beranda
+let displayPriceWd;
+let displayPriceWe;
 
 // Villa A
 {
   const villaA = new Price();
   villaA.basicPrice = 1000000;
-  villaA.percentWd = 0.1;
-  villaA.percentWe = 0.2;
-  let displayPriceWd = villaA.weekDay();
-  let displayPriceWe = villaA.weekEnd();
-  prWdA.innerHTML = displayPriceWd;
+  villaA.percentWd = 3;
+  villaA.percentWe = 3.3;
+  displayPriceWd = villaA.weekDay();
+  displayPriceWe = villaA.weekEnd();
+  prWdA.innerHTML = displayPriceWd.toLocaleString("id-ID");
+  prWeA.innerHTML = displayPriceWe.toLocaleString("id-ID");
   console.table(displayPriceWd, displayPriceWe, prWdA);
 }
