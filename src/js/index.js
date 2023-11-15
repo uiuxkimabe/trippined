ef620bc312b8e544e2d2f3734e0c1f0b230c8c01
uiuxkimabe <uiuxkimabe@gmail.com>
2023-11-15 08:12:04 +0700

    set price

--- src/js/index.js
+++ src/js/index.js
@@ -5,31 +5,37 @@ hamBtn.addEventListener("click", () => {
   navigator.classList.toggle("open");
 });
 
-/*? no js js needed from me */
+// Masukan Rincian Villa Dengan Memanggil Funtion dengan Parameter Berikut
+// ** params = nama villa (tanpa spasi)
+// ** basic = harga villa
+// ** comWd = komisi week day villa
+// ** comWe = komisi week end villa
+// ** index = nomor index villa
 
-// Harga ditampilkan di beranda dan detail villa
-// displayHarga(variable , harga modal, index, commWd,commWe)
+// Contoh : showPrice(villax,1000000,2,2.5,0)
 
-// Villa A
-const villaA = new Price();
-displayHarga(villaA, 1000000, 0, 2, 2.3);
-
-// Villa B
-const villaB = new Price();
-displayHarga(villaB, 1500000, 1, 1.8, 2.2);
-
-// Villa C
-const villaC = new Price();
-displayHarga(villaC, 1800000, 2, 2, 2.1);
-
-// Villa D
-const villaD = new Price();
-displayHarga(villaD, 600000, 3, 1.5, 2.1);
-
-// Villa E
-const villaE = new Price();
-displayHarga(villaE, 1000000, 4, 1.5, 2.1);
-
-// Villa F
-const villaF = new Price();
-displayHarga(villaE, 1000000, 5, 2, 2.1);
+// Tampilkan Harga di Beranda
+{
+  const villaA = new Price();
+  showPrice(villaA, 1000000, 2, 2.5, 0);
+}
+{
+  const villaB = new Price();
+  showPrice(villaB, 1500000, 2, 2.5, 1);
+}
+{
+  const villaC = new Price();
+  showPrice(villaC, 1200000, 2, 2.5, 2);
+}
+{
+  const villaD = new Price();
+  showPrice(villaD, 900000, 2, 2.5, 3);
+}
+{
+  const villaE = new Price();
+  showPrice(villaE, 1300000, 2, 2.5, 4);
+}
+{
+  const villaF = new Price();
+  showPrice(villaF, 1100000, 2, 2.5, 5);
+}
--- src/js/main-price.js
+++ src/js/main-price.js
@@ -1,52 +1,34 @@
 // main price berisi class untuk wadah harga modal
 
 class Price {
-  basicPrice;
-  percentWd;
-  percentWe;
-  sellPrice;
-  guideCommision = 150000;
+  basicPrice; // Harga Dasar Villa
+  comWd; // Komisi Hari Biasa
+  comWe; // Komisi Hari WeekEnd
+  sellPrice; // Harga Jual (Setelah dikali Komisi)
+  guideCommision = 150000; // Komisi Tetap Bagi Guider
   // Harga Week Day
   weekDay() {
-    this.sellPrice = this.basicPrice * this.percentWd + this.guideCommision;
+    this.sellPrice = this.basicPrice * this.comWd + this.guideCommision;
     return this.sellPrice;
   }
   // Harga Week End
   weekEnd() {
-    this.sellPrice = this.basicPrice * this.percentWe + this.guideCommision;
+    this.sellPrice = this.basicPrice * this.comWe + this.guideCommision;
     return this.sellPrice;
   }
 }
 
-// DITAMPILKAN PADA HALAMAN BERANDA
-const priceWeekDay = document.querySelectorAll(".caption .priceWd");
-const priceWeekEnd = document.querySelectorAll(".caption .priceWe");
-let mainDisplayWd;
-let mainDisplayWe;
+// MENAMPILKAN HARGA VILLA - VILLA DIBERANDA
+const hargaWd = document.querySelectorAll(".priceWd");
+const hargaWe = document.querySelectorAll(".priceWe");
 
-// RINCIAN FUNCTION DISPLAY HARGA
-const nameId = document.querySelector(".title h3").textContent;
-const villaName = ["Villa A", "Villa B", "Villa C", "Villa D"];
-let rmDtlWd = document.querySelector(".title .priceWd");
-let rmDtlWe = document.querySelector(".title .priceWe");
-
-// param = berisikan variable baru untuk memanggil class dengan isi new Price()
-// price = berisi harga dasara villa [wajib diisi]
-// index = berisi index villa yang di seleksi index start [0 - tanpa batas] [wajib isi]
-// comWd = berisi persentase komisi yang diinginkan untuk weekday [2 - 2.1] [wajib isi]
-// comWe = berisi persentase komisi yang diinginkan untuk weekend [wajib isi]
-
-function displayHarga(param, price, index, comWd, comWe) {
-  param = new Price();
-  param.basicPrice = price;
-  param.percentWd = comWd;
-  param.percentWe = comWe;
-  mainDisplayWd = param.weekDay();
-  mainDisplayWe = param.weekEnd();
-  priceWeekDay[index].innerHTML = mainDisplayWd.toLocaleString("id-ID");
-  priceWeekEnd[index].innerHTML = mainDisplayWe.toLocaleString("id-ID");
-  if (nameId === villaName[index]) {
-    rmDtlWd.innerHTML = mainDisplayWd.toLocaleString("id-ID");
-    rmDtlWe.innerHTML = mainDisplayWe.toLocaleString("id-ID");
-  }
+function showPrice(params, basic, comWd, comWe, index) {
+  params = new Price();
+  params.basicPrice = basic;
+  params.comWd = comWd;
+  params.comWe = comWe;
+  let showWd = params.weekDay();
+  let showWe = params.weekEnd();
+  hargaWd[index].innerHTML = showWd.toLocaleString("id-ID");
+  hargaWe[index].innerHTML = showWe.toLocaleString("id-ID");
 }
