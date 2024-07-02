e5fdbf4a5996cab2f3c00eea3d4cfc4ccd1c39b6
uiuxkimabe <uiuxkimabe@gmail.com>
2024-07-02 20:46:59 +0700

    update menu btn

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
 /*? no js js needed from me */
+
+const hamBtn = document.querySelector('.hamBtn')
+const menu = document.querySelector('nav .menu')
+hamBtn.addEventListener('click',()=> menu.classList.toggle('show'))
