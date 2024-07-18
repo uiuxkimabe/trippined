c356232a5ba1579bf2150b0b7773fa5d3504e968
uiuxkimabe <uiuxkimabe@gmail.com>
2024-07-19 06:50:13 +0700

    add car

--- src/js/calling.js
+++ src/js/calling.js
@@ -1,26 +1,5 @@
 // Click Menu
-menuClick()
+// menuClick()
 
 // List Villa
 
-// Villa kiki
-const kiki = new ListVilla({
-    numb: 1,
-    name: "Villa Kiki",
-    host: "Raniah Nurfadilah",
-    type: "standard",
-    priceWd: 1000000,
-    priceWe: 1500000
-})
-kiki.unit()
-
-// Villa Rani
-const rani = new ListVilla({
-    numb: 2,
-    name: "Villa Rani",
-    host: "Kiki Afrianto",
-    type: "standard",
-    priceWd: 1000000,
-    priceWe: 1500000
-})
-rani.unit()
--- src/js/index.js
+++ src/js/index.js
@@ -4,68 +4,27 @@
 
 const hamBtn = document.querySelector('.hamBtn')
 const menu = document.querySelector('nav .menu')
-function menuClick() {
-    hamBtn.addEventListener('click',()=> menu.classList.toggle('show'))
-}
 
 
-// Class Villa
-
-class ListVilla {
-    constructor({numb,name,host,type,priceWd,priceWe}) {
-        this.villaNumber = numb
-        this.villaName = name;
-        this.guide = host
-        this.type = type
-        this.priceWd = priceWd
-        this.priceWe = priceWe
+document.addEventListener('click', (e) => {
+    if (!hamBtn.contains(e.target)) {
+        menu.classList.remove('show')
+    } else {
+        menu.classList.add('show')
     }
+});
 
-    unit() {
-        // Membuat Card Villa
-        const pinnedOn = document.querySelector('.list-villa')
-        const box = document.createElement('div')
-        pinnedOn.append(box)
-        box.setAttribute('class', `all ${this.type}`)
-        box.style.border = '1px solid black'
-        
-        // Membuat Isi Card Villa
-        // Menambahkan Image
-        const boxImg = document.createElement('img')
-        box.append(boxImg)
-        boxImg.setAttribute('src', `./src/assets/villa-img/${this.villaNumber}/1.jpg`)
-        let imgLoc = `img-villa-${this.villaNumber}`
-        boxImg.setAttribute('class',`${imgLoc}`)
+const cardAll = document.querySelectorAll('.list-villa .card')
+const villaName = document.querySelectorAll('.card .villa-name')
+const cardDetail = document.querySelector('#detail')
+const close = document.querySelector('.close')
+cardAll.forEach(element => {
+    element.addEventListener('click', () => {
+        cardDetail.classList.add('show');
+    })
+});
 
-        // Menambahkan Deskripsi
-        const boxCaption = document.createElement('figcaption')
-        boxCaption.setAttribute('class', 'd-flex caption')
-        const pinnedImg = document.querySelector(`.${imgLoc}`)
-        pinnedImg.after(boxCaption)
-        
-        const villaTitle = document.createElement('h2')
-        const textVillaTitle = document.createTextNode(`${this.villaName}`)
-        villaTitle.appendChild(textVillaTitle)
-
-        const host = document.createElement('p')
-        const hostTex = document.createTextNode(`${this.guide}`)
-        host.append(hostTex)
+close.addEventListener('click', () => {
+    cardDetail.classList.remove('show');
     
-        boxCaption.appendChild(villaTitle)
-        boxCaption.appendChild(host)
-        
-        // Menambahkan Harga
-        const boxPrice = document.createElement('div')
-        boxPrice.setAttribute('class', 'price')
-        const pinnedPrice = document.querySelector('.caption')
-        pinnedPrice.after(boxPrice)
-        const priceText = document.createElement('h3')
-        const priceWd = document.createTextNode(`${this.priceWd}`)
-        boxPrice.appendChild(priceText)
-        priceText.appendChild(priceWd)
-
-        box.addEventListener('click',(e)=> alert(`${e}`))
-    }
-}
-
-
+})
